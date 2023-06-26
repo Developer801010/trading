3e452dcd7929ed092306b36982bc88ee58d3c111
Developer801010 <100435750+Developer801010@users.noreply.github.com>
2023-06-26 17:09:24 -0400

    register part fix
    register part

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -69,7 +69,6 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        dd($data);
         return User::create([
             'first_name' => $data['first_name'],
             'last_name' => $data['last_name'],
