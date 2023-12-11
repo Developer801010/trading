0d38839fa3f112da35cae7d93419c62689ce3fdf
Solmon9099 <maximovic9099@gmail.com>
2023-12-11 23:24:57 +0800

    fix bug

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Support\Facades\Auth;
 
-class RedirectIfNotAuthenticated
+class RedirectIfAuthenticated
 {
     /**
      * Handle an incoming request.
