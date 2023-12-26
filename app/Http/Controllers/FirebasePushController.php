ebbf3402b968f23da81d0b2ebe6fea98bfab5726
Developer801010 <100435750+Developer801010@users.noreply.github.com>
2023-12-26 15:21:50 -0500

    firebase debug
    firebase debug

--- app/Http/Controllers/FirebasePushController.php
+++ app/Http/Controllers/FirebasePushController.php
@@ -73,7 +73,7 @@ class FirebasePushController extends Controller
 
     public function notificationToAllMobiles($data)
     {
-        dd($data);
+        
         $title = $data['title'];
         $body =  $data['body']['first_title'].
         ' ' .  $data['body']['trade_entry_date'] .
@@ -84,7 +84,7 @@ class FirebasePushController extends Controller
         ' ' . $data['body']['visit'];
 
         $users = User::whereNotNull('fcm_token')->get()->all();
-
+        dd($users);
         foreach($users as $user){
             $message = CloudMessage::fromArray([
                 'token' => $user->fcm_token,
