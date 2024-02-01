d1ef92efaa30377c6626b455c55ea79ab0be2cf5
Developer801010 <100435750+Developer801010@users.noreply.github.com>
2024-02-01 11:38:50 -0500

    main feed update

--- app/Http/Controllers/Front/PositionmanagementController.php
+++ app/Http/Controllers/Front/PositionmanagementController.php
@@ -56,8 +56,8 @@ class PositionManagementController extends Controller
             't.created_at',
             't.updated_at'
         ])
-        ->groupBy( 't.id', 't.trade_type', 't.entry_date', 't.trade_symbol', 't.trade_direction', 't.trade_option',
-       't.strike_price', 't.entry_price', 't.stop_price', 't.target_price', 't.position_size',
+        ->groupBy( 't.id', 't.trade_type', 't.entry_date', 't.trade_symbol', 't.trade_title', 't.current_price', 't.company_name',
+         't.trade_direction', 't.trade_option', 't.strike_price', 't.entry_price', 't.stop_price', 't.target_price', 't.position_size',
         't.exit_price', 't.exit_date', 't.trade_description', 't.chart_image', 't.close_comment',
         't.close_image',  't.expiration_date', 't.created_at', 't.updated_at');
 
@@ -179,8 +179,8 @@ class PositionManagementController extends Controller
         ->where('trade_type', 'stock')
         ->whereNotNull('exit_price')
         ->whereNotNull('exit_date')
-        ->groupBy( 't.id', 't.trade_type', 't.trade_symbol', 't.trade_direction', 't.trade_option',  't.entry_date',  't.company_name',
-       't.strike_price', 't.entry_price', 't.stop_price', 't.target_price', 't.position_size',
+        ->groupBy( 't.id', 't.trade_type', 't.trade_symbol', 't.trade_direction', 't.trade_title', 't.current_price', 
+        't.trade_option',  't.entry_date', 't.company_name', 't.strike_price', 't.entry_price', 't.stop_price', 't.target_price', 't.position_size',
         't.exit_price', 't.exit_date', 't.trade_description', 't.chart_image', 't.close_comment',
         't.close_image', 't.created_at', 't.updated_at');
 
