a15d6d4930598511c0bd77a64a39e82c96f691d1
jay <mail@tretanz.com>
2024-02-26 15:21:39 +0530

    missing file for subscription

--- resources/views/front/subscription.blade.php
+++ resources/views/front/subscription.blade.php
@@ -4,8 +4,6 @@
 @section('page-style')
 
 @endsection
-
-
 @section('content')
     <div class="container">
         <section class="subscription-section">
@@ -28,90 +26,96 @@
                     </ul>
                 </div>
             </div>
-            <div class="subscription_row">
-                <div class="subscription_box">
-                    <p class="title text-center">Monthly</p>
-                    <p class="price text-center"><span class="subscription_price">$147</span>/mo</p>
-                    <div class="content">
-                        <ul>
-                            <li>
-                                Average of 2-4 easy to follow real-time trade alerts per week*
-                            </li>
-                            <li>
-                                All entries and exits delivered in real-time via SMS and Email
-                            </li>
-                            <li>
-                                Standard price
-                            </li>
-                        </ul>
-                    </div>
-                    @auth
-                        @if (count($activeSubscription) > 0)
-                            <a href="#" class="btn_subscribe">Subscribed</a>
+            <div class="subscription_row row">
+                <div class="col-12 col-md-6 col-xl-4 mb-5">
+                    <div class="subscription_box">
+                        <p class="title text-center">Monthly</p>
+                        <p class="price text-center"><span class="subscription_price">$147</span>/mo</p>
+                        <div class="content">
+                            <ul>
+                                <li>
+                                    Average of 2-4 easy to follow real-time trade alerts per week*
+                                </li>
+                                <li>
+                                    All entries and exits delivered in real-time via SMS and Email
+                                </li>
+                                <li>
+                                    Standard price
+                                </li>
+                            </ul>
+                        </div>
+                        @auth
+                            @if (count($activeSubscription) > 0)
+                                <a href="#" class="btn_subscribe">Subscribed</a>
+                            @else
+                                <a href="{{ route('front.checkout', 'm') }}" class="btn_subscribe btn_subscribe_popular">Subscribe</a>
+                            @endif
                         @else
-                            <a href="{{ route('front.checkout', 'm') }}" class="btn_subscribe btn_subscribe_popular">Subscribe</a>
-                        @endif
-                    @else
-                        <a href="{{ route('front.checkout', 'm') }}" class="btn_subscribe">Subscribe</a>
-                    @endauth
+                            <a href="{{ route('front.checkout', 'm') }}" class="btn_subscribe">Subscribe</a>
+                        @endauth
 
+                    </div>
                 </div>
 
-                <div class="subscription_box subsscription_box_popular">
-                    <p class="popular_badge">Most Popular</p>
-                    <p class="title text-center">Yearly</p>
-                    <p class="price text-center"><span class="subscription_price">$787</span>/yr</p>
-                    <div class="content">
-                        <ul>
-                            <li>
-                                Average of 2-4 easy to follow real-time trade alerts per week*
-                            </li>
-                            <li>
-                                All entries and exits delivered in real-time via SMS and Email
-                            </li>
-                            <li>
-                                Save $1,174!
-                            </li>
-                        </ul>
-                    </div>
-                    @auth
-                        @if (count($activeSubscription) > 0)
-                            <a href="#" class="btn_subscribe btn_subscribe_popular">Subscribed</a>
+                <div class="col-12 col-md-6 col-xl-4 mb-5 mt-4 mt-md-0 mt-lg-0 mt-xl-0">
+                    <div class="subscription_box subsscription_box_popular">
+                        <p class="popular_badge">Most Popular</p>
+                        <p class="title text-center">Yearly</p>
+                        <p class="price text-center"><span class="subscription_price">$787</span>/yr</p>
+                        <div class="content">
+                            <ul>
+                                <li>
+                                    Average of 2-4 easy to follow real-time trade alerts per week*
+                                </li>
+                                <li>
+                                    All entries and exits delivered in real-time via SMS and Email
+                                </li>
+                                <li>
+                                    Save $1,174!
+                                </li>
+                            </ul>
+                        </div>
+                        @auth
+                            @if (count($activeSubscription) > 0)
+                                <a href="#" class="btn_subscribe btn_subscribe_popular">Subscribed</a>
+                            @else
+                                <a href="{{ route('front.checkout', 'y') }}" class="btn_subscribe btn_subscribe_popular">Subscribe</a>
+                            @endif
                         @else
                             <a href="{{ route('front.checkout', 'y') }}" class="btn_subscribe btn_subscribe_popular">Subscribe</a>
-                        @endif
-                    @else
-                        <a href="{{ route('front.checkout', 'y') }}" class="btn_subscribe btn_subscribe_popular">Subscribe</a>
-                    @endauth
+                        @endauth
 
+                    </div>
                 </div>
 
-                <div class="subscription_box">
-                    <p class="title text-center">Quarterly</p>
-                    <p class="price text-center"><span class="subscription_price">$387</span>/qu</p>
-                    <div class="content">
-                        <ul>
-                            <li>
-                                Average of 2-4 easy to follow real-time trade alerts per week*
-                            </li>
-                            <li>
-                                All entries and exits delivered in real-time via SMS and Email
-                            </li>
-                            <li>
-                                Save $336!
-                            </li>
-                        </ul>
-                    </div>
-                    @auth
-                        @if (count($activeSubscription) > 0)
-                            <a href="#" class="btn_subscribe">Subscribed</a>
+                <div class="col-12 col-md-6 col-xl-4 mb-5">
+                    <div class="subscription_box">
+                        <p class="title text-center">Quarterly</p>
+                        <p class="price text-center"><span class="subscription_price">$387</span>/qu</p>
+                        <div class="content">
+                            <ul>
+                                <li>
+                                    Average of 2-4 easy to follow real-time trade alerts per week*
+                                </li>
+                                <li>
+                                    All entries and exits delivered in real-time via SMS and Email
+                                </li>
+                                <li>
+                                    Save $336!
+                                </li>
+                            </ul>
+                        </div>
+                        @auth
+                            @if (count($activeSubscription) > 0)
+                                <a href="#" class="btn_subscribe">Subscribed</a>
+                            @else
+                                <a href="{{ route('front.checkout', 'q') }}" class="btn_subscribe btn_subscribe_popular">Subscribe</a>
+                            @endif
                         @else
-                            <a href="{{ route('front.checkout', 'q') }}" class="btn_subscribe btn_subscribe_popular">Subscribe</a>
-                        @endif
-                    @else
-                        <a href="{{ route('front.checkout', 'q') }}" class="btn_subscribe">Subscribe</a>
-                    @endauth
+                            <a href="{{ route('front.checkout', 'q') }}" class="btn_subscribe">Subscribe</a>
+                        @endauth
 
+                    </div>
                 </div>
             </div>
             <p class="short-description text-center">(All memberships renew automatically - cancel anytime)</p>
